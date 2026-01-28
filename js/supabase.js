d8e176bd721abd43f2ae6c46ca4062d1ce834068
syarifaharifah22-prog <syarifaharifah22@gmail.com>
2026-01-28 19:11:18 +0700

    ls -a

--- js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://zclcbusqbttvzyhhnrti.supabase.co";
-const SUPABASE_KEY = "sb_publishable_F6fdeEqRppFS_3d64ij9Gg_WnpOUksa";
-
-const sb = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
